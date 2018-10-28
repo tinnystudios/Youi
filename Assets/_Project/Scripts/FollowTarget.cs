26d74d6b0807179779ce0aa7850d90c52e46655c
tinnystudios <tinnystudios@gmail.com>
2018-10-28 12:05:15 -0700

    Isometric Prototype

---
+++ Assets/_Project/Scripts/FollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform Target;
+    public float Speed = 3;
+    public float distance = 5;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        var newPosition = Target.position;
+        newPosition -= transform.forward * distance;
+
+        transform.position = Vector3.Slerp(transform.position,newPosition,Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _Speed = 2;
+    [SerializeField] private float _JumpForce = 3;
+
+    [SerializeField] private Rigidbody _Rb;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, 0, y) * Time.deltaTime * _Speed;
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        _Rb.velocity = new Vector3(0, _JumpForce, 0);
+    }
+}
